7989a1675b9ca65e9f512d129d2998e598fd873b
David King <davidk@openismus.com>
2009-07-27 15:26:42 +0200

    Add autogen.sh for those unschooled in <5 year old autotools

---
+++ autogen.sh
@@ -0,0 +1,11 @@
+#! /bin/sh -e
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+
+autoreconf --force --install "$srcdir"
+
+echo "--"
+echo "autoreconf --force --install"
+echo "--"
+echo "Remember it well, because one day, autogen.sh may be replaced with an"
+echo "even smaller shell script."
