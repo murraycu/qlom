f74efa56584279e5669af7285638e1ca6e1b1250
David King <davidk@openismus.com>
2009-07-28 16:29:50 +0200

    Make autogen.sh run configure

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,7 @@ test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 
 autoreconf --force --install "$srcdir"
+"$srcdir/configure" "$@"
 
 echo "--"
 echo "autoreconf --force --install"
