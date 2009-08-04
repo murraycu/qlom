74f56fb7cbf6a88efe06023fa84db97323494f6e
Murray Cumming <murrayc@murrayc.com>
2009-08-04 16:40:23 +0200

    Primitive command-line parsing instead of hard-coding the filepath
    Signed-off-by: David King <davidk@openismus.com>

--- src/qlom.cc
+++ src/qlom.cc
@@ -32,15 +32,33 @@ int main(int argc, char **argv)
   QCoreApplication::setOrganizationDomain("openismus.com");
   QCoreApplication::setApplicationName(PACKAGE_TARNAME);
 
+  // TODO: Confirm that Qt has not equivalent to GOption,
+  // meaning no way to specify option details (short name, long name, description, type).
+  // TODO: I keep hearing that STL containers can be used with Qt, but how would we use std::list<> here instead of QStringList? murrayc. 
+  QStringList options = app.arguments();
+  if(options.size() < 2)
+  {
+    std::cout << "Usage: glom filepath" << std::endl;
+    return 1;
+  }
+  
+  QString filepath_qt = options[1];
+  
+  //TODO: toStdString() converts UTF-8 to ASCII, suggesting that it will corrupt 
+  //some filepaths. Avoid that - we just want a raw char*.
+  std::string filepath = filepath_qt.toStdString();
+  //Note that QString cannot be used directly with std::cout.
+  //std::cout << "DEBUG: filepath=" << filepath << std::endl;
+  
   // TODO: Find a Qt way to get a URI from a filename:
   std::string uri;
   try
   {
-    uri = Glib::filename_to_uri("/opt/gnome2/share/glom/doc/examples/example_music_collection.glom");
+    uri = Glib::filename_to_uri(filepath);
   }
   catch(const Glib::ConvertError& ex)
   {
-    std::cerr << "Exception from Glib::filename_to_uri(): " << ex.what();
+    std::cerr << "Exception from Glib::filename_to_uri(): " << ex.what() << std::endl;
     return 1;
   }
   
