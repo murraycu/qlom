211d591aeb5be77649cdfc34310caf0661fe9257
Michael Hasselmann <michaelh@openismus.com>
2009-10-16 18:37:55 +0200

    Add a simplistic SQL query builder
    The QlomQueryBuilder was created to allow a simple reordering of columns
    before the model hits the table view. It might also be useful to deal with
    the conversion problems for numbers.
    
    Signed-off-by: David King <davidk@openismus.com>

---
+++ src/query_builder.cc
@@ -0,0 +1,82 @@
+/* Qlom is copyright Openismus GmbH, 2009
+ *
+ * This file is part of Qlom
+ *
+ * Qlom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Qlom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Qlom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "query_builder.h"
+
+QlomQueryBuilder::QlomQueryBuilder()
+{}
+
+QlomQueryBuilder::~QlomQueryBuilder()
+{}
+
+void QlomQueryBuilder::addProjection(const QString& projection)
+{
+    Q_ASSERT(projection != 0 && !projection.isEmpty());
+
+    projections.push_back(projection);
+}
+
+void QlomQueryBuilder::addRelation(const QString& relation)
+{
+    Q_ASSERT(relation != 0 && !relation.isEmpty());
+
+    relations.push_back(relation);
+}
+
+void QlomQueryBuilder::equiJoinWith(const QString& relation,
+    const QString& lhs, const QString& rhs)
+{
+    Q_ASSERT(relation != 0 && !relation.isEmpty() && lhs != 0 && !lhs.isEmpty()
+        && rhs != 0 && !rhs.isEmpty());
+
+    QString builder =
+        QString("JOIN %1 ON (%2 = %3) ").arg(relation).arg(lhs).arg(rhs);
+
+    equiJoins.push_back(builder);
+}
+
+void QlomQueryBuilder::setSelection(const QString& theSelection)
+{
+    Q_ASSERT(theSelection != 0 && !theSelection.isEmpty());
+
+    selection = QString("WHERE (%1) ").arg(theSelection);
+}
+
+void QlomQueryBuilder::setOrderBy(const QString& theOrderBy)
+{
+    Q_ASSERT(theOrderBy != 0 && !theOrderBy.isEmpty());
+
+    orderBy = QString("ORDER BY (%1)").arg(theOrderBy);
+}
+
+QString QlomQueryBuilder::getQuery(bool distinct) const
+{
+    Q_ASSERT(!projections.isEmpty() && !relations.isEmpty());
+
+    QString builder;
+
+    builder.push_back(
+        QString((!distinct ? "SELECT %1": "SELECT DISTINCT %1"))
+        .arg(projections.join(", ")));
+    builder.push_back(QString(" FROM %1 ").arg(relations.join(", ")));
+    builder.push_back(equiJoins.join(" "));
+    builder.push_back(selection);
+    builder.push_back(orderBy);
+
+    return builder;
+}
---
+++ src/query_builder.h
@@ -0,0 +1,80 @@
+/* Qlom is copyright Openismus GmbH, 2009
+ *
+ * This file is part of Qlom
+ *
+ * Qlom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Qlom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Qlom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef QLOM_QUERY_BUILDER_H_
+#define QLOM_QUERY_BUILDER_H_
+
+#include <QString>
+#include <QStringList>
+
+/** A simplistic SQL query builder.
+ *  This class represents a very simplistic SQL query builder, to create
+ *  queries that can fill a QSqlTableModel without losing flexibility. It tries
+ *  to stick to SQL-99, but for now the only "officially" supported backend
+ *  will be SQLite, since Qlom targets the Maemo platform.
+ */
+class QlomQueryBuilder
+{
+public:
+
+    /** Create a bare SQL query builder. */
+    QlomQueryBuilder();
+    ~QlomQueryBuilder();
+
+    /** Add a projection to the query.
+    *  For example, "SELECT [DISTINCT] projection".
+    *  @param[in] projection a projection string to add */
+    void addProjection(const QString& projection);
+
+    /** Add a relation to the query.
+    *  For example, "FROM rel1 [,rel2]".
+    *  @param[in] relation the relation string to add */
+    void addRelation(const QString& relation);
+
+    /** Add an equi-join to the query.
+    *  For example, "JOIN rel ON lhs = rhs".
+    *  @param[in] relation the relation string to add
+    *  @param[in] lhs the left-hand side of the join
+    *  @param[in] rhs the right-hand side of the join */
+    void equiJoinWith(const QString& relation, const QString& lhs,
+        const QString& rhs);
+
+    /** Set the selection of the query.
+    *  For example, "WHERE selection".
+    *  @param[in] selection the selection string to add */
+    void setSelection(const QString& selection);
+
+    /** Set the order clause of the query.
+    *  For example, "ORDER BY order_by".
+    *  @param[in] order_by the order clause to add */
+    void setOrderBy(const QString& orderBy);
+
+    /** Returns the built query, but does not check for validity.
+    *  @param[in] distinct whether to add the distinct keyword to the query */
+    QString getQuery(bool distinct = false) const;
+
+private:
+
+  QStringList projections; /**< the list of projections */
+  QStringList relations; /**< the list of relations */
+  QStringList equiJoins; /**< the list of qui-joins */
+  QString selection; /**< the selection */
+  QString orderBy; /**< the order clause */
+};
+
+#endif /* QLOM_QUERY_BUILDER_H_ */
