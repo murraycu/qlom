358bb95321052fa737d5714d55401915d2353d54
David King <davidk@openismus.com>
2009-11-17 16:23:20 +0100

    Use qCritical() instead of std::cerr in GlomDocument

--- src/glom_document.cc
+++ src/glom_document.cc
@@ -55,7 +55,7 @@ bool GlomDocument::loadDocument(const QString &filepath, std::auto_ptr<QlomError
     int failure_code = 0;
     const bool test = document->load(failure_code);
     if(!test) {
-        std::cerr << "Document loading failed with uri=" << uri << std::endl;
+        qCritical("Document loading failed with uri %s", uri.c_str());
         error = std::auto_ptr<QlomError>(
             new QlomError(tr("libglom failed to load the Glom document"),
                 Qlom::CRITICAL_ERROR_SEVERITY));
@@ -65,7 +65,7 @@ bool GlomDocument::loadDocument(const QString &filepath, std::auto_ptr<QlomError
     /* Check that the document is not an example document, which must be
      * resaved — that would be an extra feature. */
     if(document->get_is_example_file()) {
-        std::cerr << "Document is an example file, cannot process" << std::endl;
+        qCritical("Document is an example file, cannot process");
         error = std::auto_ptr<QlomError>(new QlomError(
             tr("Cannot open the document because it is an example file"),
             Qlom::CRITICAL_ERROR_SEVERITY));
@@ -100,7 +100,7 @@ bool GlomDocument::loadDocument(const QString &filepath, std::auto_ptr<QlomError
     case Glom::Document::HOSTING_MODE_POSTGRES_SELF:
     // Fall through.
     default:
-        std::cerr << "Database type not supported, cannot process" << std::endl;
+        qCritical("Database type not supported, cannot process");
         error = std::auto_ptr<QlomError>(new QlomError(
             tr("Database type not supported, failed to open the Glom document"),
             Qlom::CRITICAL_ERROR_SEVERITY));
@@ -140,8 +140,8 @@ std::string GlomDocument::filepathToUri(const QString &theFilepath,
         uri = Glib::filename_to_uri(filepath);
     }
     catch(const Glib::ConvertError& convertException) {
-        std::cerr << "Exception from Glib::filename_to_uri(): "
-            << convertException.what() << std::endl;
+        qCritical("Exception from Glib::filename_to_uri(): %s",
+            convertException.what().c_str());
         error = std::auto_ptr<QlomError>(new QlomError(
             tr("Failed to convert the document file name to a URI"),
             Qlom::CRITICAL_ERROR_SEVERITY));
@@ -150,8 +150,8 @@ std::string GlomDocument::filepathToUri(const QString &theFilepath,
     std::auto_ptr<Glib::Error> convertError;
     uri = Glib::filename_to_uri(filepath, convertError);
     if(convertError.get()) {
-        std::cerr << "Error from Glib::filename_to_uri(): "
-            << convertError->what() << std::endl;
+        qCritical("Error from Glib::filename_to_uri(): %s",
+            convertError->what());
         error = std::auto_ptr<QlomError>(new QlomError(
             tr("Failed to convert the document file name to a URI"),
             Qlom::CRITICAL_ERROR_SEVERITY));
@@ -169,9 +169,8 @@ bool GlomDocument::openSqlite()
     const QSqlError error(db.lastError());
     if (error.isValid()) {
         // TODO: Give feedback in the UI.
-        std::cerr << "Database backend \"" << backend.toUtf8().constData()
-          << "\" does not exist\n" << "Error details: "
-          << error.text().toUtf8().constData() << std::endl;
+        qCritical("Database backend \"%s\" does not exist\nError details: %s",
+            backend.toUtf8().constData(), error.text().toUtf8().constData());
         return false;
     }
 
@@ -184,8 +183,8 @@ bool GlomDocument::openSqlite()
                 document->get_connection_self_hosted_directory_uri())));
     }
     catch (const Glib::ConvertError &convertException) {
-        std::cerr << "Exception from Glib::filename_to_uri(): "
-            << convertException.what() << std::endl;
+        qCritical("Exception from Glib::filename_to_uri(): %s",
+            convertException.what().c_str());
         return false;
     }
 #else /* !GLIBMM_EXCEPTIONS_ENABLED */
@@ -194,16 +193,16 @@ bool GlomDocument::openSqlite()
         document->get_connection_self_hosted_directory_uri(),
         convertError));
     if (convertError.get()) {
-        std::cerr << "Error from Glib::filename_from_uri(): "
-            << convertError->what() << std::endl;
+        qCritical("Error from Glib::filename_from_uri(): %s",
+            convertError->what());
         return false;
     }
 
     filename = ustringToQstring(Glib::filename_to_utf8(stdFilename,
         convertError));
     if (convertError.get()) {
-        std::cerr << "Error from Glib::filename_to_utf8(): "
-            << convertError->what() << std::endl;
+        qCritical("Error from Glib::filename_to_utf8(): %s",
+            convertError->what());
         return false;
     }
 #endif /* GLIBMM_EXCEPTIONS_ENABLED */
@@ -217,12 +216,12 @@ bool GlomDocument::openSqlite()
     if (sqliteDb.exists()) {
         db.setDatabaseName(filename);
     } else {
-        std::cerr << "Sqlite database does not exist" << std::endl;
+        qCritical("Sqlite database does not exist");
         return false;
     }
 
     if (!db.open()) {
-        std::cerr << "Database connection could not be opened" << std::endl;
+        qCritical("Database connection could not be opened");
         return false;
     } else {
         return true;
